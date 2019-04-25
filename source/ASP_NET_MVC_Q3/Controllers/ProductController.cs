14f0895940239a4e13f42ea3a8b36a01d6eacb9f
suec <suec@bqool.com>
2019-04-25 19:26:06 +0800

    可修改資料，但修改前的資料會並存

--- source/ASP_NET_MVC_Q3/Controllers/ProductController.cs
+++ source/ASP_NET_MVC_Q3/Controllers/ProductController.cs
@@ -46,24 +46,44 @@ namespace ASP_NET_MVC_Q3.Controllers
             }
             return View("Add");
         }
-        public ActionResult Edit(int Id, string Name, string Locale, DateTime CreateDate, DateTime UpdateDate)
+        public ActionResult Edit(int id)
         {
-            DataViewModel dataViewModel = new DataViewModel();
-            dataViewModel.product = new Product();
-            dataViewModel.product.CreateDate = CreateDate;
-            dataViewModel.product.Id = Id;
-            dataViewModel.product.Name = Name;
-            dataViewModel.product.Locale = Locale;
-            dataViewModel.product.UpdateDate = UpdateDate;
-            return View(dataViewModel);
+            Product product = new Product();
+            foreach (var item in source)
+            {
+                if (item.Id == id)
+                {
+                    product.Id = item.Id ;
+                    product.Locale = item.Locale ;
+                    product.Name = item.Name;
+                    product.CreateDate = item.CreateDate ;
+                }
+            }
+            var itemlist = new List<SelectListItem>();
+            itemlist.Add(new SelectListItem { Text = "Unite State", Value = "US", Selected = true });
+            itemlist.Add(new SelectListItem { Text = "Germany", Value = "EU" });
+            itemlist.Add(new SelectListItem { Text = "Canada", Value = "CA" });
+            itemlist.Add(new SelectListItem { Text = "Spain", Value = "ES" });
+            itemlist.Add(new SelectListItem { Text = "France", Value = "FR" });
+            itemlist.Add(new SelectListItem { Text = "Japen", Value = "JP" });
+            ViewBag.CategoryID = itemlist;
+            var time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            product.UpdateDate = DateTime.Parse(time);
+            
+            return View(product);
         }
         [HttpPost]
         public ActionResult Edit(Product product)
         {
 
-            return View(product);
+            if (ModelState.IsValid)
+            {         
+                source.Add(new Product() { CreateDate = product.CreateDate, Id = product.Id, Locale =product.Locale, Name = product.Name,UpdateDate=product.UpdateDate });
+                return View("List", source);
+            }
+            return View("Edit");
         }
-        public ActionResult Delete()
+        public ActionResult Delete(Product product)
         {
             List<Product> source = Product.Data;
             return View(source);
